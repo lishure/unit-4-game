52a883db56f00f209d0ca99cc934756a4bac6f7b
Lishure <HUEY_LISA@YAHOO.COM>
2019-03-20 19:41:17 -0700

    Added images and layout

---
+++ assets/javascript/game.js
@@ -0,0 +1,25 @@
+    
+    
+    //Need to find 4 images for the crystal visuals.
+   //Need to create 4 button tags for the images.
+
+   
+    //Need to use mathrandom for the random number between 19-120.
+    //Maybe need an array? or not?
+
+   
+    //Need to make an add function for the 4 crystals.- need random number for 4 crystals between 1-12.
+    
+     //hide random number, use, on click event to show random number when buttons are clicked
+
+ 
+        //Need if and else statement for wins
+  
+        //If and else statement if score goes above random number
+
+   //automatic restart game
+
+    
+//when game restarts, score counter reset to 0. 4 new values for crystals. 
+  
+//track wins and loses.
